7184a44857d6c347af4c8b80a246b73816f75cdd
Mostafa <mostayasser61@gmail.com>
2023-07-13 14:14:28 -0700

    f

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
- /**
- * main - main Entry point
- * Description: A C program that  print with put function
- * Return: Always 0 (success)
- */
+ 
 int main(void)
-{	puts("\"Programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main entry point
+ * Description: function that print a string
+ * Return:0 Always  (sucess)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
