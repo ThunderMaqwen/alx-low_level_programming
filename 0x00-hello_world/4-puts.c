080b74ce59600d57a922b86a72327114d8136731
Mostafa <mostayasser61@gmail.com>
2023-07-13 13:45:40 -0700

    mm

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point for practice
+ * description: A C function puts print string
+ * return always integer 0 (success)
+ */
+
+
+
+
+
+int main(void)
+{	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
