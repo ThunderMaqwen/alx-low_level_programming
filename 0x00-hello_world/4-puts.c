6f9a1230652c9fc5af9dfd2628ec92dd5ed4c8f5
Mostafa <mostayasser61@gmail.com>
2023-07-13 13:58:53 -0700

    kareka

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
-/**
- * main - entry point for practice
- * description: A C function puts print string
- * return always integer 0 (success)
+ /**
+ * main - main Entry point
+ * Description: A C program that  print with put function
+ * Return: Always 0 (success)
  */
-
-
-
-
-
 int main(void)
 {	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
