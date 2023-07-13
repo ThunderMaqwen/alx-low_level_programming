2c0fc6c9ea67892fecd0248252e8c1d5ed9c7b08
Mostafa <mostayasser61@gmail.com>
2023-07-13 14:33:01 -0700

    vv

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main entry point
+ * Description: functions that print the data types
+ * Return:0 Always (sucess)
+ */
+int main(void)
+{
+	printf("Size of a char: %c byte(s)", sizeof(char));
+	printf("Size of a int: %i byte(s)", sizeof(int));
+	printf("Size of a long int: %li byte(s)", sizeof(long int));
+	printf("Size of a long long int: %lli byte(s)", sizeof(long long int));
+	printf("Size of a float: %f byte(s)", sizeof(float));
+	return (0);
+}
