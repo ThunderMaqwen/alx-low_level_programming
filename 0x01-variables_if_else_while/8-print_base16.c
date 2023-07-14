002de885e90e0873c61c4c0c97d560ccc007bd45
Mostafa <mostayasser61@gmail.com>
2023-07-14 12:00:58 -0700

    cc

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-main entry point
+ * Description: print hexadecimal
+ * Return:0 Always (success)
+*/
+int main(void)
+{
+	char ch = 12;
+
+	while (ch <=
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main-main entry point
+ * Description:print numbers
+ * Return:0 Always (success)
+*/
+int main(void)
+{
+	char ch = '0'
+
+		while (ch <= '9')
+		{
+			putchar(ch - 48", ");
+			ch++;
+		}
+	putchar('\n');
+	return (0);
+}
