305b7a5a1d2f2794a642c3328e1384efadaed1f3
Mostafa <mostayasser61@gmail.com>
2023-07-14 11:26:00 -0700

    hhj

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main-main entry point
+ * Description:print numbers
+ * Return:0 Always (success)
+*/
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	return (0);
+}
