3c6fd2f21a4213f081cae8f67cf35ec4400feb5b
Mostafa <mostayasser61@gmail.com>
2023-07-14 09:33:56 -0700

    bb

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main-main entry point
+ * Description:printing alphapet
+ * Return:0 Always (success)
+*/
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		putchar(x);
+		x++;
+	}
+	putchar("\n");
+	return (0);
+}
