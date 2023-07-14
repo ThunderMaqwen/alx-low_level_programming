8ecbeb367fa0acbb7a19eed4cf6718ed643dee86
Mostafa <mostayasser61@gmail.com>
2023-07-14 11:09:05 -0700

    cc

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main-main entry point
+ * Description:print alphpet lowercase and upper
+ * Return:0 Always (success)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	putchar ch = 'a';
+	putchar x = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar("%c", ch);
+		ch++;
+	}
+	putchar('\n');
+	while (x <= 'Z')
+	{
+		putchar("%c", x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
